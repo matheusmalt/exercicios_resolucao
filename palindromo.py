59547550574df84cb346bda0cfac48549b071d5a
Matheus Malta <95448895+matheusmalt@users.noreply.github.com>
2023-02-01 00:55:12 -0300

    Create palindromo.py

---
+++ palindromo.py
@@ -0,0 +1,43 @@
+'''
+Crear un sistema que detecte si una palabra es palíndroma o no
+
+Para solucionar este problema el usuario tiene que ingresar por pantalla una palabra y el sistema verificas si es palíndromo o no.
+
+Una palabra palíndroma se lee de igual formo como de la derecha y de la izquierda.
+'''
+import sys
+
+def palindromo(palavra):
+    reverso = palavra[::-1]
+    if palavra == reverso:
+        print("São palindromos")
+    else:
+        print("Não são palindromos")
+
+menu = """
+
+
+                     __  __                  __                                             
+                    /  |/  |                /  |                                            
+  ______    ______  $$ |$$/  _______    ____$$ |  ______    ______   _____  ____    ______  
+ /      \  /      \ $$ |/  |/       \  /    $$ | /      \  /      \ /     \/    \  /      \ 
+/$$$$$$  | $$$$$$  |$$ |$$ |$$$$$$$  |/$$$$$$$ |/$$$$$$  |/$$$$$$  |$$$$$$ $$$$  |/$$$$$$  |
+$$ |  $$ | /    $$ |$$ |$$ |$$ |  $$ |$$ |  $$ |$$ |  $$/ $$ |  $$ |$$ | $$ | $$ |$$ |  $$ |
+$$ |__$$ |/$$$$$$$ |$$ |$$ |$$ |  $$ |$$ \__$$ |$$ |      $$ \__$$ |$$ | $$ | $$ |$$ \__$$ |
+$$    $$/ $$    $$ |$$ |$$ |$$ |  $$ |$$    $$ |$$ |      $$    $$/ $$ | $$ | $$ |$$    $$/ 
+$$$$$$$/   $$$$$$$/ $$/ $$/ $$/   $$/  $$$$$$$/ $$/        $$$$$$/  $$/  $$/  $$/  $$$$$$/  
+$$ |                                                                                        
+$$ |                                                                                        
+$$/                                                                                         
+
+"""
+def main():
+    try:
+        print(menu)
+        palavra = input("\nDigite a palavra: ")
+        palindromo(palavra)
+    except:
+        print("Erro")
+        sys.exit()
+
+main()
